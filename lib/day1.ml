2d51471afaf6f732929f008668d94eff9877bc99
Thomas Nelson <tanelso2@gmail.com>
2023-12-01 11:04:40 -0600

    Solved day1

--- lib/day1.ml
+++ lib/day1.ml
@@ -1,7 +1,64 @@
+open Utils
+
+let get_digits s =
+    let s' = String.to_seq s |> List.of_seq in
+    List.filter Base.Char.is_digit s'
+
+let first_digit s =
+    List.hd s |> Base.Char.get_digit_exn
+
+let last_digit s =
+    s |> List.rev |> List.hd |> Base.Char.get_digit_exn
+
+let get_score s =
+    let d = get_digits s in
+    let first = first_digit d in
+    let last = last_digit d in
+    (first * 10) + last
+
+let calc_scores s =
+    let lines = Base.String.split_lines s in
+    let scores = List.map get_score lines in
+    ListUtils.sum scores
+
+let translate (s:char list) : char list =
+  let rec helper s acc =
+      let continue x =
+          helper (List.tl s) (x::acc)
+      in
+      match s with
+      | 'o'::'n'::'e'::_ -> continue '1'
+      | 't'::'w'::'o'::_ -> continue '2'
+      | 't'::'h'::'r'::'e'::'e'::_ -> continue '3'
+      | 'f'::'o'::'u'::'r'::_ -> continue '4'
+      | 'f'::'i'::'v'::'e'::_ -> continue '5'
+      | 's'::'i'::'x'::_ -> continue '6'
+      | 's'::'e'::'v'::'e'::'n'::_ -> continue '7'
+      | 'e'::'i'::'g'::'h'::'t'::_ -> continue '8'
+      | 'n'::'i'::'n'::'e'::_ -> continue '9'
+      | x::_ -> continue x
+      | [] -> List.rev acc
+  in
+  helper s []
+
+let get_digits2 s =
+    let s' = String.to_seq s |> List.of_seq in
+    let s'' = translate s' in
+    List.filter Base.Char.is_digit s''
+
+let get_score2 s =
+    let d = get_digits2 s in
+    let first = first_digit d in
+    let last = last_digit d in
+    (first * 10) + last
+
+let calc_scores2 s =
+    let lines = Base.String.split_lines s in
+    let scores = List.map get_score2 lines in
+    ListUtils.sum scores
+
 let part1 (s:string) =
-    let _ = s in
-    failwith "NOIMPL"
+    calc_scores s
 
 let part2 (s:string) =
-    let _ = s in
-    failwith "NOIMPL"
+    calc_scores2 s
--- test/test_day1.ml
+++ test/test_day1.ml
@@ -7,14 +7,58 @@ let part1' () = Option.try_with (fun () -> part1 test_input)
 
 let part2' () = Option.try_with (fun () -> part2 test_input)
 
+let%expect_test "get_score" =
+    let show_score s =
+        Stdio.printf "%d\n" (get_score s)
+    in
+    show_score "1abc2";
+    [%expect {| 12 |}];
+    show_score "pqr3stu8vwx";
+    [%expect {| 38 |}];
+    show_score "a1b2c3d4e5f";
+    [%expect {| 15 |}]
+
+let%expect_test "get_score2" =
+    let show_score s =
+        Stdio.printf "%d\n" (get_score2 s)
+    in
+    show_score "two1nine";
+    [%expect {| 29 |}];
+    show_score "eightwothree";
+    [%expect {| 83 |}];
+    show_score "7pqrstsixteen";
+    [%expect {| 76 |}];
+    show_score "zoneight234";
+    [%expect {| 14 |}];
+    show_score "abcone2threexyz";
+    [%expect {| 13 |}];
+    show_score "xtwoone3four";
+    [%expect {| 24 |}];
+    show_score "eighthree";
+    [%expect {| 83 |}];
+    show_score "sevenine";
+    [%expect {| 79 |}];
+    let sample_string = {|two1nine
+      eightwothree
+      abcone2threexyz
+      xtwone3four
+      4nineeightseven2
+      zoneight234
+      7pqrstsixteen|} in
+    Stdio.printf "%d\n" (calc_scores2 sample_string);
+    [%expect {| 281 |}]
+
+
+
+
 let%expect_test "part1" =
     (match part1' () with
      | None -> ()
      | Some x -> Stdio.printf "%d\n" x);
-    [%expect {| |}]
+    [%expect {| 54601 |}]
 
 let%expect_test "part2" =
     (match part2' () with
      | None -> ()
      | Some x -> Stdio.printf "%d\n" x);
-    [%expect {| |}]
+    [%expect {| 54078 |}]
